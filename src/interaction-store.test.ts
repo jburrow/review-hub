9305d933c12d7c3811fef435911717febea82bcf
James Burrow <jaburrow@gmail.com>
2020-03-02 21:29:14 +0000

    more tests

---
+++ src/interaction-store.test.ts
@@ -0,0 +1,26 @@
+import { interactionReducer } from "./interaction-store";
+
+test("interaction-store: selectedCommitId", () => {
+  const store = interactionReducer(
+    { currentUser: "" },
+    { type: "selectCommit", commitId: "cid" }
+  );
+
+  expect(store.selectedCommitId).toBe("cid");
+});
+
+test("interaction-store: selectedView", () => {
+  const selectedView = {
+    readOnly: false,
+    label: "",
+    fullPath: "",
+    revision: 0,
+    text: ""
+  };
+  const store = interactionReducer(
+    { currentUser: "" },
+    { type: "selectedView", ...selectedView }
+  );
+
+  expect(store.selectedView).toEqual(selectedView);
+});
