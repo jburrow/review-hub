39e778a2f30414437648508b74292e37683c8a75
James Burrow <jaburrow@gmail.com>
2020-04-13 22:04:48 +0100

    pre-publish

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,14 +1,3 @@
-import * as React from "react";
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
 import "./index.css";
-interface Persistence {
-    save: () => void;
-    load: () => void;
-}
-export declare const App: React.ComponentType<Pick<{
-    classes: Record<"layout" | "version_control" | "editor" | "script_history" | "vc_history" | "panel_content" | "panel_heading", string>;
-} & {
-    persistence?: Persistence;
-}, "persistence"> & import("@material-ui/core").StyledComponentProps<"layout" | "version_control" | "editor" | "script_history" | "vc_history" | "panel_content" | "panel_heading">>;
-export {};
